4212e607d8fb1f1ad5bec0aff43f55b68b468141
RC-Regalado <ruben.alekey@gmail.com>
2019-02-03 18:04:24 -0600

    Main form

---
+++ Tarea_1/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Tarea_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mtxName = new System.Windows.Forms.MaskedTextBox();
+            this.cboSex = new System.Windows.Forms.ComboBox();
+            this.cboStatus = new System.Windows.Forms.ComboBox();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.mtxMail = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mtxDui = new System.Windows.Forms.MaskedTextBox();
+            this.mtxNit = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cboAppoinment = new System.Windows.Forms.ComboBox();
+            this.dtpContractDate = new System.Windows.Forms.DateTimePicker();
+            this.cboDepartament = new System.Windows.Forms.ComboBox();
+            this.mtxPointment = new System.Windows.Forms.MaskedTextBox();
+            this.mtxSalary = new System.Windows.Forms.MaskedTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nmbre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha de nacm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dreccn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "crre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "DU";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(299, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "NT";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Fecha de cntra";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 323);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Puntaje";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(306, 316);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Sueld";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 235);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Carg";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(287, 230);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Departament";
+
+
+            // 
+            // maskedTextBox1
+            // 
+            this.mtxName.Location = new System.Drawing.Point(103, 12);
+            this.mtxName.Name = "maskedTextBox1";
+            this.mtxName.Size = new System.Drawing.Size(100, 20);
+            this.mtxName.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.cboSex.FormattingEnabled = true;
+            this.cboSex.Items.AddRange(new object[] {
+                                                    "Masculino",
+                                                    "Femenino"});
+            this.cboSex.Location = new System.Drawing.Point(239, 12);
+            this.cboSex.Name = "comboBox1";
+            this.cboSex.Size = new System.Drawing.Size(89, 21);
+            this.cboSex.TabIndex = 5;
+            this.cboSex.Text = "Sexo";
+            // this.cboSex.TextChanged += new System.EventHandler(OnClickSend);
+            // 
+            // comboBox2
+            // 
+            this.cboStatus.FormattingEnabled = true;
+            this.cboStatus.Location = new System.Drawing.Point(374, 12);
+            this.cboStatus.Name = "comboBox2";
+            this.cboStatus.Size = new System.Drawing.Size(119, 21);
+            this.cboStatus.TabIndex = 6;
+            this.cboStatus.Text = "Estado Civil";
+            // 
+            // comboBox3
+            // 
+            this.cboAppoinment.FormattingEnabled = true;
+            this.cboAppoinment.Location = new System.Drawing.Point(103, 227);
+            this.cboAppoinment.Name = "comboBox3";
+            this.cboAppoinment.Size = new System.Drawing.Size(121, 21);
+            this.cboAppoinment.TabIndex = 15;
+            this.cboAppoinment.Text = "Cargo";
+            // 
+            // textBox1
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(103, 98);
+            this.txtAddress.Name = "textBox1";
+            this.txtAddress.Size = new System.Drawing.Size(241, 20);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // maskedTextBox2
+            // 
+            this.mtxMail.Location = new System.Drawing.Point(103, 141);
+            this.mtxMail.Name = "maskedTextBox2";
+            this.mtxMail.Size = new System.Drawing.Size(239, 20);
+            this.mtxMail.TabIndex = 9;
+            
+            // 
+            // maskedTextBox3
+            // 
+            this.mtxDui.Location = new System.Drawing.Point(103, 184);
+            this.mtxDui.Name = "maskedTextBox3";
+            this.mtxDui.Size = new System.Drawing.Size(100, 20);
+            this.mtxDui.TabIndex = 12;
+            // 
+            // maskedTextBox4
+            // 
+            this.mtxNit.Location = new System.Drawing.Point(374, 175);
+            this.mtxNit.Name = "maskedTextBox4";
+            this.mtxNit.Size = new System.Drawing.Size(100, 20);
+            this.mtxNit.TabIndex = 13;
+            // 
+            // dateTimePicker2
+            // 
+            this.dtpContractDate.Location = new System.Drawing.Point(103, 271);
+            this.dtpContractDate.Name = "dateTimePicker2";
+            this.dtpContractDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpContractDate.TabIndex = 17;
+            // 
+            // comboBox4
+            // 
+            this.cboDepartament.FormattingEnabled = true;
+            this.cboDepartament.Location = new System.Drawing.Point(372, 227);
+            this.cboDepartament.Name = "comboBox4";
+            this.cboDepartament.Size = new System.Drawing.Size(121, 21);
+            this.cboDepartament.TabIndex = 18;
+            // 
+            // maskedTextBox5
+            // 
+            this.mtxPointment.Location = new System.Drawing.Point(103, 316);
+            this.mtxPointment.Name = "maskedTextBox5";
+            this.mtxPointment.Size = new System.Drawing.Size(100, 20);
+            this.mtxPointment.TabIndex = 19;
+            // 
+            // maskedTextBox6
+            // 
+            this.mtxSalary.Location = new System.Drawing.Point(380, 316);
+            this.mtxSalary.Name = "maskedTextBox6";
+            this.mtxSalary.Size = new System.Drawing.Size(100, 20);
+            this.mtxSalary.TabIndex = 20;
+
+            // 
+            // button1
+            // 
+            this.btnSend.Location = new System.Drawing.Point(48, 364);
+            this.btnSend.Name = "button1";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 25;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.OnClickSend);
+            // 
+            // button2
+            // 
+            this.btnReset.Location = new System.Drawing.Point(214, 364);
+            this.btnReset.Name = "button2";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 26;
+            this.btnReset.Text = "Reiniciar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.OnClickReset);
+            // 
+            // button3
+            // 
+            this.btnExit.Location = new System.Drawing.Point(380, 364);
+            this.btnExit.Name = "button3";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 27;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.OnClickClose);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 400);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSend);
+            
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+
+            this.Controls.Add(this.mtxSalary);
+            this.Controls.Add(this.mtxPointment);
+            this.Controls.Add(this.mtxNit);
+            this.Controls.Add(this.mtxDui);
+            this.Controls.Add(this.mtxMail);
+            this.Controls.Add(this.mtxName);
+
+            this.Controls.Add(this.txtAddress);
+            
+            this.Controls.Add(this.cboDepartament);
+            this.Controls.Add(this.cboStatus);
+            this.Controls.Add(this.cboSex);
+            this.Controls.Add(this.cboAppoinment);
+
+            this.Controls.Add(this.dtpContractDate);
+            this.Controls.Add(this.dtpBirth);
+            
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        //
+        private System.Windows.Forms.DateTimePicker dtpContractDate;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        //
+        private System.Windows.Forms.MaskedTextBox mtxMail;
+        private System.Windows.Forms.MaskedTextBox mtxDui;
+        private System.Windows.Forms.MaskedTextBox mtxNit;
+        private System.Windows.Forms.MaskedTextBox mtxName;
+        private System.Windows.Forms.MaskedTextBox mtxPointment;
+        private System.Windows.Forms.MaskedTextBox mtxSalary;
+        //
+        private System.Windows.Forms.ComboBox cboAppoinment;
+        private System.Windows.Forms.ComboBox cboDepartament;
+        private System.Windows.Forms.ComboBox cboStatus;
+        private System.Windows.Forms.ComboBox cboSex;
+        //
+        private System.Windows.Forms.TextBox txtAddress;
+        //
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Tarea_1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Tarea_1
+{
+    delegate void Backend (object data);
+    public partial class Form1 : Form
+    {
+        
+        private Backend log = data  => Console.WriteLine(data);
+        private Backend error = data => Console.WriteLine("Error: {0}", data);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickSend(object sender, EventArgs args)
+        {
+            log(cboSex.Text.ToString());
+            bool ok = true;
+
+            foreach (Control control in Controls)
+            {
+                string name = control.GetType().Name;
+                
+                
+                switch(name)
+                {
+                    case "ComboBox":{
+                        ok = ComboBoxHandler((ComboBox)control);
+                    }
+                    break;
+
+                    default:
+                    {
+
+                    }
+                    break;
+                }
+                
+                if (!ok) OnError(control);
+            }
+
+
+
+            // this.Close();
+        }
+
+        private void OnClickClose(object sender, EventArgs args) => this.Close();
+
+        private void OnClickReset(object sender, EventArgs args)
+        {
+
+        }
+
+        private void OnError(Control control) => control.ForeColor = Color.Red;
+
+        private bool ComboBoxHandler(ComboBox cboItem)
+        {
+            log(cboItem.Text);
+
+            return true;
+        }
+    }
+}
